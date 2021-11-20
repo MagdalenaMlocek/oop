b3d280d3cecefa3d401f958beb779ad1916d6cdb
Magda <magdalena.mlocek20@gmail.com>
2021-11-20 11:48:50 +0100

    oop

---
+++ object_programming/src/Car.java
@@ -0,0 +1,10 @@
+public class Car {
+    public void stopEngine(){
+        System.out.println("Silnik nie dziala");
+
+    }
+    public void startEngine(){
+        System.out.println("Silnik działa");
+
+    }
+}
